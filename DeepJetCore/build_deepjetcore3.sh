5fc02078b8725c7a07fb1e854940693a89c797b5
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2020-10-23 15:25:52 +0200

    Update

--- DeepJetCore/build_deepjetcore3.sh
+++ DeepJetCore/build_deepjetcore3.sh
@@ -3,6 +3,6 @@
 export SINGULARITY_CACHEDIR=/local/$USER/singularity_cache
 export SINGULARITY_TMPDIR=/local/$USER/singularity_tmp
 
-OUTPUT_DIR=/local/HephyML/singularity
+OUTPUT_DIR=/local/HephyML/singularity/images
 
 singularity build $OUTPUT_DIR/deepjetcore3-latest.sif docker://cernml4reco/deepjetcore3:latest
--- DeepJetCore/run_deepjetcore3.sh
+++ DeepJetCore/run_deepjetcore3.sh
@@ -1,4 +1,36 @@
-#! /bin/sh
+#! /bin/sh -x
 
+# command line
 
-singularity shell -B /afs -B /local -B /data -B /scratch --nv deepjetcore3-latest.sif
+if [ $# -le 1 ]
+then
+   cmd='shell'
+else
+   cmd='run'
+fi
+
+# determine binds for singularity
+
+sopts="--nv"
+for mp in /scratch /scratch-cbe /local /data /afs
+do
+   if [ -e $mp ]
+   then
+      sopts="-B $mp $sopts"
+   fi
+done
+
+# singularity image on different installations
+
+if [ $(hostname) = "hepgpu01.hephy.oeaw.ac.at" ]
+then
+   images="/local/HephyML/singularity/images"
+elif [ $(hostname -d) = "vbc.ac.at" ]
+then
+   images="/mnt/hephy/HephyML/singularity/images"
+else
+   echo "Unknown host"
+   exit 1
+fi
+
+singularity $cmd $sopts $images/deepjetcore3-latest.sif
